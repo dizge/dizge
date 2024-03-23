1ae8788359202d2bd2cad8149546dab39c0dfa7f
Mehmet Umut Mutlu <mehmetumutmutlu@gmail.com>
2024-03-23 22:56:01 +0300

    v0.1.6: Minor bug fixes

--- setup.py
+++ setup.py
@@ -10,13 +10,12 @@ README = (HERE / "README.md").read_text(encoding="utf-8")
 # This call to setup() does all the work
 setup(
     name="dizge",
-    version="0.1.5",
+    version="0.1.6",
     description="The grammar analyzer for Turkish",
     long_description=README,
     long_description_content_type="text/markdown",
     packages=find_packages(),
     url="https://github.com/dizge/dizge",
-    author="Mehmet Umut Mutlu, Nazlıcan Yetimaslan, İlker Atagün",
     author_email="dizgenlp@gmail.com",
     license="MIT",
     keywords=['python', 'nlp', 'linguistics', 'phonology', "turkish", "grammar"],
